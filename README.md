5fb7c20d9e4827a57c29bea6310cbb05ecce1f84
Freddie Hoy <freddiehoy@hotmail.com>
2023-06-28 17:03:54 +0100

    Change from proxy to seperate endpoint

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Documentation
 
-Turns out I have no idea what I'm doing. I wote a bunch of code 2 months ago and have now completly forgotten how I did it and what it does...
-
-So I think I'm going to start writing a coding diary.
+Writing a code diary
 
 #### Big issues
 
--- src/useApi.tsx
+++ src/useApi.tsx
@@ -1,12 +1,15 @@
 import axios from "axios";
 
+const backendBaseURL = "http://localhost:8000";
+
 export const useApi = () => {
   const instance = axios.create({
-    baseURL: "",
+    baseURL: backendBaseURL,
     timeout: 1000,
     headers: {
       "Content-Type": "application/json",
     },
+    withCredentials: true,
   });
   return instance;
 };
